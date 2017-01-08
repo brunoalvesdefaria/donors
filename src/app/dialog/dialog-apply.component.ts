968fcd724cb29c818b9eebc019ceae35f3ab1759
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2017-01-08 17:30:46 -0200

    Creating inputs for donor application modal

--- src/app/dialog/dialog-apply.component.ts
+++ src/app/dialog/dialog-apply.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './dialog-apply.component.html',
   styleUrls: ['./dialog-apply.component.css']
 })
-export class DialogApplyComponent { }
+export class DialogApplyComponent {
+
+  bloodTypes = [
+    {value: 'op', viewValue: 'O+'},
+    {value: 'on', viewValue: 'O-'},
+    {value: 'bp', viewValue: 'B+'},
+    {value: 'bn', viewValue: 'B-'},
+    {value: 'ap', viewValue: 'A+'},
+    {value: 'an', viewValue: 'A-'},
+    {value: 'abp', viewValue: 'AB+'},
+    {value: 'abn', viewValue: 'AB-'}
+  ];
+
+}
