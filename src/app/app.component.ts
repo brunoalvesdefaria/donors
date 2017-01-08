161c0f435b7438da22fc90c2eec1d3bb973d02a6
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2017-01-08 10:22:13 -0200

    Adding socket.io on client-side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as io from "socket.io-client";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BloodMap';
+  socket = null;
+
+  ngOnInit() {
+    this.socket = io.connect('http://localhost');
+    this.socket.on('news', function (data) {
+      console.log(data);
+      this.socket.emit('my other event', { my: 'data' });
+    });
+  }
 }
